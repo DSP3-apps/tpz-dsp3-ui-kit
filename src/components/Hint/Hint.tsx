47453dd0d7a85c3e4349299568a00e19869152df
nagatpz <Nagapentamanaidu.Mahanti@external.telespazio.com>
2025-03-07 04:39:35 +0000

    removed class block from the styles of Hint component, because it div element, by default it displays in block.

--- src/components/Hint/Hint.tsx
+++ src/components/Hint/Hint.tsx
@@ -6,7 +6,7 @@ export type HintProps = ExtendProps<'div'>;
 
 export const Hint = ({ className, children, ...props }: HintProps) => {
   return (
-    <div className={twMerge('block mb-2 text-lg text-text-secondary', className)} {...props}>
+    <div className={twMerge('mb-2 text-lg text-text-secondary', className)} {...props}>
       {children}
     </div>
   );
