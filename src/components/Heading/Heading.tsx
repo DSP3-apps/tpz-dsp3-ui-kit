e3dbc83b2f5ff14600a4e6e2f187e2dd38756a09
Rishab Mehra <rishab.mehra@external.telespazio.com>
2025-06-18 08:16:06 +0100

    fix(components): Add className prop to Heading component
    Introduced a className prop to the Heading component to apply
    custom CSS styles.
    
    IssueID DCI-236

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,21 +1,48 @@
+import { twMerge } from 'tailwind-merge';
+
 export type HeadingProps = {
   type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   children: React.ReactNode;
+  className?: string;
 };
 
-export const Heading = ({ type, children }: HeadingProps) => {
+export const Heading = ({ type, children, className }: HeadingProps) => {
   switch (type) {
     case 'h1':
-      return <h1 className="py-4 text-4xl font-bold text-text-primary">{children}</h1>;
+      return (
+        <h1 className={twMerge('text-text-primary py-4 text-4xl font-bold', className)}>
+          {children}
+        </h1>
+      );
     case 'h2':
-      return <h2 className="py-3 text-3xl font-bold text-text-primary">{children}</h2>;
+      return (
+        <h2 className={twMerge('text-text-primary py-3 text-3xl font-bold', className)}>
+          {children}
+        </h2>
+      );
     case 'h3':
-      return <h3 className="py-3 text-xl font-bold text-text-primary">{children}</h3>;
+      return (
+        <h3 className={twMerge('text-text-primary py-3 text-xl font-bold', className)}>
+          {children}
+        </h3>
+      );
     case 'h4':
-      return <h4 className="py-3 text-lg font-bold text-text-primary">{children}</h4>;
+      return (
+        <h4 className={twMerge('text-text-primary py-3 text-lg font-bold', className)}>
+          {children}
+        </h4>
+      );
     case 'h5':
-      return <h5 className="py-2 text-base font-bold text-text-primary">{children}</h5>;
+      return (
+        <h5 className={twMerge('text-text-primary py-2 text-base font-bold', className)}>
+          {children}
+        </h5>
+      );
     case 'h6':
-      return <h6 className="py-2 text-sm font-bold text-text-primary">{children}</h6>;
+      return (
+        <h6 className={twMerge('text-text-primary py-2 text-sm font-bold', className)}>
+          {children}
+        </h6>
+      );
   }
 };
