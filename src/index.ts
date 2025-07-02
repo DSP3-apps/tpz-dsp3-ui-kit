3b8a727826fcacc7ceba820c309d536ef5973e1e
Mark Small <mark.small@telespazio.com>
2025-07-02 16:33:43 +0100

    feat(components): Add dropdown/select menu component
    This component is to be used by the forth-coming Header component,
    I want to ensure though that I don't try to commit too much in one.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Button } from './components/Button/Button';
 export { Card } from './components/Card/Card';
 export { Heading } from './components/Heading/Heading';
 export { Paragraph } from './components/Paragraph/Paragraph';
+export { DropdownMenu } from './components/dropdown/DropdownMenu';
